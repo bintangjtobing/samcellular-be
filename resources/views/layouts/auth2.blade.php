ed3a098a5d1dfdc3210b7f03858702f89502ddf6
Bintang Tobing <hello@bintangtobing.com>
2025-01-23 16:48:48 +0700

    Refactor sidebar and authentication styles for improved readability and layout consistency

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -10,6 +10,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="https://res.cloudinary.com/dilb4d364/image/upload/v1737624495/1024logo_witxkl.png"
+        type="image/png" sizes="512x512">
 
     <title>@yield('title') - {{ config('app.name', 'POS') }}</title>
 
@@ -28,16 +30,17 @@
 <body class="pace-done" data-new-gr-c-s-check-loaded="14.1172.0" data-gr-ext-installed="" cz-shortcut-listen="true">
     @inject('request', 'Illuminate\Http\Request')
     @if (session('status') && session('status.success'))
-        <input type="hidden" id="status_span" data-status="{{ session('status.success') }}"
-            data-msg="{{ session('status.msg') }}">
+    <input type="hidden" id="status_span" data-status="{{ session('status.success') }}"
+        data-msg="{{ session('status.msg') }}">
     @endif
     <div class="container-fluid">
         <div class="row eq-height-row">
-            <div class="col-md-12 col-sm-12 col-xs-12 right-col tw-pt-20 tw-pb-10 tw-px-5">
+            <div class="col-md-12 col-sm-12 col-xs-12 tw-pt-20 tw-pb-10 tw-px-5">
                 <div class="row">
                     <div
                         class="lg:tw-w-16 md:tw-h-16 tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-mx-auto tw-overflow-hidden tw-bg-white tw-rounded-full tw-p-0.5 tw-mb-4">
-                        <img src="{{ asset('img/logo-small.png')}}" alt="lock" class="tw-rounded-full tw-object-fill" />
+                        <img src="https://res.cloudinary.com/dilb4d364/image/upload/v1737624495/1024logo_witxkl.png"
+                            alt="lock" class="tw-rounded-full tw-object-fill" />
                     </div>
 
                     <div class="tw-absolute tw-top-2 md:tw-top-5 tw-left-4 md:tw-left-8 tw-flex tw-items-center tw-gap-4"
@@ -45,38 +48,40 @@
                         @include('layouts.partials.language_btn')
 
                         @if(Route::has('repair-status'))
-                            <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
-                                href="{{ action([\Modules\Repair\Http\Controllers\CustomerRepairStatusController::class, 'index']) }}">
-                                @lang('repair::lang.repair_status')
-                            </a>
+                        <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
+                            href="{{ action([\Modules\Repair\Http\Controllers\CustomerRepairStatusController::class, 'index']) }}">
+                            @lang('repair::lang.repair_status')
+                        </a>
                         @endif
                     </div>
 
                     <div class="tw-absolute tw-top-5 md:tw-top-8 tw-right-5 md:tw-right-10 tw-flex tw-items-center tw-gap-4"
                         style="text-align: left">
                         @if (!($request->segment(1) == 'business' && $request->segment(2) == 'register'))
-                            <!-- Register Url -->
-                            @if (config('constants.allow_registration'))
-                            {{-- <span
-                                class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base">{{ __('business.not_yet_registered') }}
-                            </span> --}}
+                        <!-- Register Url -->
+                        @if (config('constants.allow_registration'))
+                        {{-- <span class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base">{{
+                            __('business.not_yet_registered') }}
+                        </span> --}}
 
-                            <div class="tw-border-2 tw-border-white tw-rounded-full tw-h-10 md:tw-h-12 tw-w-24 tw-flex tw-items-center tw-justify-center">
-                             <a href="{{ route('business.getRegister')}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif"
-                                    class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white">
-                                    {{ __('business.register') }}</a>
-                            </div>
+                        <div
+                            class="tw-border-2 tw-border-white tw-rounded-full tw-h-10 md:tw-h-12 tw-w-24 tw-flex tw-items-center tw-justify-center">
+                            <a href="{{ route('business.getRegister')}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif"
+                                class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white">
+                                {{ __('business.register') }}</a>
+                        </div>
 
-                                <!-- pricing url -->
-                                @if (Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-                                    &nbsp; <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
-                                        href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a>
-                                @endif
-                            @endif
+                        <!-- pricing url -->
+                        @if (Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
+                        &nbsp; <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
+                            href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a>
+                        @endif
+                        @endif
                         @endif
                         @if ($request->segment(1) != 'login')
-                            <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
-                                href="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'login'])}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif">{{ __('business.sign_in') }}</a>
+                        <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
+                            href="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'login'])}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif">{{
+                            __('business.sign_in') }}</a>
                         @endif
                     </div>
                     <div class="col-md-10 col-xs-8" style="text-align: right;">
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,16 +1,20 @@
 <!-- Left side column. contains the logo and sidebar -->
-<aside class="side-bar tw-relative tw-hidden tw-h-full tw-bg-white tw-w-64 xl:tw-w-64 lg:tw-flex lg:tw-flex-col tw-shrink-0">
+<aside
+    class="side-bar tw-relative tw-hidden tw-h-full tw-bg-white tw-w-64 xl:tw-w-64 lg:tw-flex lg:tw-flex-col tw-shrink-0">
 
     <!-- sidebar: style can be found in sidebar.less -->
 
     {{-- <a href="{{route('home')}}" class="logo">
-		<span class="logo-lg">{{ Session::get('business.name') }}</span>
-	</a> --}}
+        <span class="logo-lg">{{ Session::get('business.name') }}</span>
+    </a> --}}
 
     <a href="{{route('home')}}"
         class="tw-flex tw-items-center tw-justify-center tw-w-full tw-border-r tw-h-15 tw-bg-@if(!empty(session('business.theme_color'))){{session('business.theme_color')}}@else{{'primary'}}@endif-800 tw-shrink-0 tw-border-primary-500/30">
         <p class="tw-text-lg tw-font-medium tw-text-white side-bar-heading tw-text-center">
-            {{ Session::get('business.name') }} <span class="tw-inline-block tw-w-3 tw-h-3 tw-bg-green-400 tw-rounded-full" title="Online"></span>
+            <div class="col-xs-12">
+                <img src="https://res.cloudinary.com/dilb4d364/image/upload/v1737625126/Sys_vlmcl1.png"
+                    class="img-rounded" alt="Logo" style="margin: 30px 0;">
+            </div>
         </p>
     </a>
 
